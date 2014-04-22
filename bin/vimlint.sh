316c2be9fa74d0da48388620af5b634d152fb3f5
LCD 47 <lcd047@gmail.com>
2014-04-22 22:04:24 +0300

    Overhaul of bin/vimlint.sh.  Fix .travis.yml. (#45)
    Conflicts:
    	.travis.yml
    	bin/vimlint.sh

--- bin/vimlint.sh
+++ bin/vimlint.sh
@@ -1,85 +1,53 @@
 #!/bin/sh
-set -e
+
+#PATH=/usr/local/bin:/bin:/usr/bin
 
 usage()
 {
-	if [ $# -ne 0 ]
-	then
-		echo "$@" >& 2
-	fi
-
-
-	cat <<- EOF 1>&2
-	Usage $0 [-h] file ...
-	    -h: print this message
+	cat <<EOF >&2
+Usage ${0##*/} [-p <dir>] [-l <dir>] [-h] {<file>|<dir>} ...
+ -p <dir>	look for vim-vimlparser in <dir>
+ -l <dir>	look for vim-vimlint in <dir>
+ -h		print this message and exit
 EOF
+	exit 1
 }
 
 
-OPT=
-DIR=
-FILE=/tmp/vimlint.$$.tmp
-RET=0
-VLINT=
-VPARS=
-while getopts ho:l:p: OPT
-do
-	case $OPT in
-	o)
-		FILE=$OPTARG ;;
+VOPT="-c 'set rtp+=`pwd`'"
+while getopts 'hl:p:' OPT; do
+	case "$OPT" in
 	p)
 		if [ ! -f "${OPTARG}/autoload/vimlparser.vim" ]; then
 			usage
-			exit 1
 		fi
-		VPARS="$OPTARG" ;;
+		VOPT="$VOPT -c 'set rtp+=$OPTARG'"
+		shift ;;
 	l)
 		if [ ! -f "${OPTARG}/autoload/vimlint.vim" ]; then
 			usage
-			exit 1
 		fi
-		VLINT="$OPTARG" ;;
-	d)
-		DIR=$OPTARG ;;
-	h)
-		usage
-		exit 1;;
-	\?)
-		usage "invalid option"
-		exit 1 ;;
+		VOPT="$VOPT -c 'set rtp+=$OPTARG'"
+		shift ;;
+	*)
+		usage ;;
 	esac
+	shift; OPTIND=1
 done
-shift `expr $OPTIND - 1`
 
-VOPT=""
-if [ "${VLINT}" != "" ]; then
-	VOPT="${VOPT} -c 'set rtp+=${VLINT}'"
-fi
+TF=$( mktemp -t "${0##*/}"-$$.XXXXXXXX ) || exit 1
+trap 'rm -f "$TF"' EXIT HUP INT QUIT TERM
 
-if [ "${VPARS}" != "" ]; then
-	VOPT="${VOPT} -c 'set rtp+=${VPARS}'"
-fi
-VOPT="${VOPT} -c 'set rtp+=`pwd`'"
-
-for file in "$@"
-do
-	echo "$0: start ${file}"
-	VIM="vim ${VOPT} -c 'call vimlint#vimlint(\"'${file}'\", {\"output\" : \"'${FILE}'\"})' -c 'qall!'"
-	eval ${VIM} > /dev/null 2>&1
-	if [ -f ${FILE} ]; then
-		if [ `cat ${FILE} | wc -l` -gt 0 ]; then
-			cat ${FILE}
-			if [ `cat ${FILE} | grep Error | wc -l` -gt 0 ]; then
-				RET=2
-			fi
-		fi
-		rm -f ${FILE}
+RET=0
+while [ $# -gt 0 ]; do
+	if [ -n "$1" -a \( -f "$1" -o -d "$1" \) ]; then
+		cat /dev/null >"$TF" || exit 1
+		vim $VOPT \
+			-c "call vimlint#vimlint('$1', {'quiet':  1, 'output': '$TF'})" \
+			-c 'qall!' >/dev/null 2>&1
+		egrep -w 'Error|Warning' "$TF" && RET=2
 	fi
-	echo "$0: end ${file}"
+	shift
 done
 
-echo "exit ${RET}"
-exit ${RET}
-
-# EOF
-
+exit $RET
