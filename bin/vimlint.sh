fd6841b9f9ed2f53dbc95e5e2106a72e6898440f
syngan <syn3gan@gmail.com>
2014-11-20 04:42:31 +0900

    vimlint.sh: add -c option

--- bin/vimlint.sh
+++ bin/vimlint.sh
@@ -6,16 +6,20 @@
 usage()
 {
 	cat <<EOF >&2
-Usage ${0##*/} [-p <dir>][-l <dir>][-e <EVLxxx=n>][-v][-E][-h] {<file>|<dir>} ...
+Usage ${0##*/} [-p <dir>][-l <dir>][-e errlv][-v][-E][-h][-c config] {<file>|<dir>} ...
  -p <dir>           look for vim-vimlparser in <dir>
  -l <dir>           look for vim-vimlint in <dir>
  -h                 print this message and exit
- -e EVLxxx=n        set error level
+ -e EVLxxx=n        set error level for all variables
+ -e EVLxxx.var=n    set error level for the variable "var"
                       n=1: none
                       n=3: warning
                       n=5: error
- -e EVLxxx.var=n	set error level for variable "var"
+                    e.g. -e EVL103=1
+                    e.g. -e EVL102.l:_=1
+ -c key=value       e.g. -c func_abort=1
  -E                 report only error messages
+ -v                 verbose mode
 EOF
 	exit 1
 }
@@ -24,8 +28,9 @@ EOF
 VERBOSE=0
 VOPT="-c 'set rtp+=`pwd`'"
 CONFIG="-c 'call has_key(g:, \"vimlint#config\") | let g:vimlint#config = {}'"
+CONFIG="${CONFIG} -c 'let g:vimlint#config.quiet = 1'"
 ERRGREP='Error|Warning'
-while getopts 'hl:p:e:vE' OPT; do
+while getopts 'hl:p:e:vc:E' OPT; do
 	case "$OPT" in
 	p)
 		if [ ! -f "${OPTARG}/autoload/vimlparser.vim" ]; then
@@ -57,6 +62,15 @@ while getopts 'hl:p:e:vE' OPT; do
 			usage
 		fi
 		shift ;;
+	c)
+		if [ `echo ${OPTARG} | grep '^[a-z0-9_]\+=' | wc -l` = 1 ]; then
+			K=`echo ${OPTARG} | sed 's/=.*//'`
+			V=`echo ${OPTARG} | sed 's/^[a-z0-9_]\+=//'`
+			CONFIG="$CONFIG -c 'let g:vimlint#config.$K=$V'"
+		else
+			usage
+		fi
+		shift ;;
 	v)
 		VERBOSE=1 ;;
 	*)
@@ -72,7 +86,7 @@ RET=0
 while [ $# -gt 0 ]; do
 	if [ -n "$1" -a \( -f "$1" -o -d "$1" \) ]; then
 		cat /dev/null >"$TF" || exit 1
-		VIM="vim $VOPT $CONFIG -c 'call vimlint#vimlint(\"$1\", {\"quiet\":  1, \"output\": \"${TF}\"})' -c 'qall!'"
+		VIM="vim $VOPT $CONFIG -c 'call vimlint#vimlint(\"$1\", {\"output\": \"${TF}\"})' -c 'qall!'"
 		eval ${VIM} > /dev/null 2>&1
 		if [ ${VERBOSE} = 0 ]; then
 			egrep -w "${ERRGREP}" "$TF" && RET=2
