f382e9d7db1e93e71cee98dcc296d5429b7a5a9a
syngan <syn3gan@gmail.com>
2018-07-05 21:39:15 +0900

    up feature
    +\ 'osxdarwin': 1,

--- resource/feature_list.py
+++ resource/feature_list.py
@@ -11,39 +11,37 @@ import sys
 # }}}
 
 if len(sys.argv) < 2:
-  print 'Usage: feature-list.py evalfunc.c'
+  print('Usage: feature-list.py evalfunc.c')
   sys.exit(2)
 
-fp = open(sys.argv[1])
-for line in fp:
-  if re.match('^f_has\(', line):
-    break
+with open(sys.argv[1], 'r') as fp:
+  for line in fp:
+    if re.match('^f_has\(', line):
+      break
 
-dict = {}
-for line in fp:
-  if re.match('^\s*".*",.*', line):
-    m = re.search('".*"', line)
-    line = line[m.start()+1 : m.end()-1].lower()
-    dict[line] = 1
-    if re.match('.*win32.*', line):
-      win64 = line.replace('win32', 'win64')
-      dict[win64] = 1
-  elif re.match('^\s*NULL', line):
-    break
+  dict = {}
+  for line in fp:
+    if re.match('^\s*".*",.*', line):
+      m = re.search('".*"', line)
+      line = line[m.start()+1 : m.end()-1].lower()
+      dict[line] = 1
+      if re.match('.*win32.*', line):
+        win64 = line.replace('win32', 'win64')
+        dict[win64] = 1
+    elif re.match('^\s*NULL', line):
+      break
 
-for line in fp:
-  if re.match('\s*else if \(STRICMP\(name, "[0-9a-zA-Z_]+"\) ', line):
-    m = re.search('".*"', line)
-    line = line[m.start()+1 : m.end()-1].lower()
-    dict[line] = 1
-  elif re.match('^}', line):
-    break
-
-fp.close()
+  for line in fp:
+    if re.match('\s*else if \(STRICMP\(name, "[0-9a-zA-Z_]+"\) ', line):
+      m = re.search('".*"', line)
+      line = line[m.start()+1 : m.end()-1].lower()
+      dict[line] = 1
+    elif re.match('^}', line):
+      break
 
 keys = dict.keys()
 for k in sorted(keys):
-  print "\ '{0}': 1,".format(k)
+  print("\ '{0}': 1,".format(k))
 
 
 
